1cd79b8b31a60c33b886dfb3484a7504c9673003
Sebastien Renaud <sebastien.renaud@gmail.com>
2012-03-02 23:05:41 -0500

    states defined, question text centered

--- mathers.py
+++ mathers.py
@@ -18,18 +18,26 @@ green    = (   0, 255,   0)
 red      = ( 255,   0,   0)
 yellow   = ( 255, 255,   0)
 
-# support numpad values
+# support numpad values ans some shift values
 keynum = {
- 48:'0',256:'0'
-,49:'1',257:'1'
-,50:'2',258:'2'
-,51:'3',259:'3'
-,52:'4',260:'4'
-,53:'5',261:'5'
-,54:'6',262:'6'
-,55:'7',263:'7'
-,56:'8',264:'8'
-,57:'9',265:'9'}
+ 48:['0',')'],256:['0','0']
+,49:['1','!'],257:['1','1']
+,50:['2','@'],258:['2','2']
+,51:['3','#'],259:['3','3']
+,52:['4','$'],260:['4','4']
+,53:['5','%'],261:['5','5']
+,54:['6','%'],262:['6','6']
+,55:['7','&'],263:['7','7']
+,56:['8','*'],264:['8','8']
+,57:['9','('],265:['9','9']
+,45:['-','_']
+,47:['/','?']
+,61:['=','+']
+,267:['/','/']
+,268:['*','*']
+,269:['-','-']
+,270:['+','+']
+}
 K_NUMPAD_RETURN = 271
 
 message_timeout = 1000
@@ -107,9 +115,11 @@ class Question:
         self.size = 50
             
     def answer_is_valid(self,user_input):
+        if self.unknown == 'OPERATOR' and self.second == 1:
+            if self.operator == '*' or self.operator == '/':
+                if user_input == '*' or user_input == '/':
+                    return True
         if self.valid_input == user_input:
-            self.color = green
-            self.size = 70
             return True
         else:
             self.reset_text()
@@ -134,14 +144,23 @@ class Question:
         else:
             self.text += ' %d' % self.answer
         
-        self.color = yellow
-        self.size = 40
-        
     def render(self, screen):
         if self.text:
             font = pygame.font.Font(None, self.size)
             text = font.render(self.text,True,self.color)
-            screen.blit(text, [250,250])
+            sx, sy = screen.get_size()
+            fx, fy = font.size(self.text)
+            screen.blit(text, [sx/2-fx/2,sy/2-fy/2])
+
+# states
+WAITING_FOR_INPUT = 0
+SOME_INPUT_PRESENT = 1
+ANS_SUBMITTED = 2 
+ANS_VALID = 3
+CONGRADULATE = 4
+ANS_WRONG = 5
+PUNISH = 6
+QUIT = 7
 
 class Game:
     def __init__(self):
@@ -155,20 +174,73 @@ class Game:
         self.a = None
         self.q = None
         self.message_timeout = 0
-    def update(self, user_input):
-        # check if there is a new answer
-        if self.a != None:
-            print "checking %s" % self.a
-            if self.q.answer_is_valid(self.a):
-                self.message = "BRAVO"
-                self.a = None
+        self.user_input = ''
+        self.state = WAITING_FOR_INPUT
+        
+    def process_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.state = QUIT
+                break
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE: # pressing escape quits
+                    self.state = QUIT
+                    break
+                for key, char in keynum.iteritems():
+                    if event.key == key:
+                        self.state = SOME_INPUT_PRESENT 
+                        g.user_input += keynum[key][event.mod&1]
+                if event.key == pygame.K_RETURN or event.key == K_NUMPAD_RETURN: 
+                    if g.user_input != '':
+                        self.state = ANS_SUBMITTED
+                        g.a = g.user_input
+                        break
+                if event.key == pygame.K_BACKSPACE:
+                    # erase last char of user input
+                    if len(g.user_input) <= 1:
+                        g.user_input = ''
+                        self.state = WAITING_FOR_INPUT
+                    else:                        
+                        g.user_input = g.user_input[:len(g.user_input)-1]
+                print event
+        
+        
+    def update(self):
+        # empty input queue
+        self.process_events()
+        if self.state == WAITING_FOR_INPUT:
+            self.q.reset_text()
+        elif self.state == SOME_INPUT_PRESENT:
+            self.q.color = yellow
+            self.q.size = 50
+            self.q.update(g.user_input)
+        elif self.state == ANS_SUBMITTED:
+            if self.q.answer_is_valid(g.user_input):
+                self.state = ANS_VALID
             else:
-                # Oops
-                self.message =  "OOPS"
-                self.a = None
-        else:
-            if user_input != '':
-                self.q.update(user_input)
+                self.state = ANS_WRONG
+        elif self.state == ANS_VALID:
+            self.q.size = 70
+            self.q.color = green
+            self.q.update(g.user_input)
+            self.timeout = 0
+            self.state = CONGRADULATE
+        elif self.state == CONGRADULATE:
+            self.timeout += time_chunk
+            if self.timeout > message_timeout:
+                self.q = Question()
+                g.user_input = ''
+                self.state = WAITING_FOR_INPUT
+        elif self.state == ANS_WRONG:
+            self.q.color = red
+            self.q.update(g.user_input)
+            self.timeout = 0
+            self.state = PUNISH
+        elif self.state == PUNISH:
+            self.timeout += time_chunk
+            if self.timeout > message_timeout:
+                g.user_input = ''
+                self.state = WAITING_FOR_INPUT
             
     def render(self):
         self.scr.fill(black)
@@ -178,8 +250,6 @@ class Game:
             self.scr.blit(text, [250,350])
             self.message_timeout += time_chunk
             if self.message_timeout > message_timeout:
-                if self.message == "BRAVO":
-                    self.q = Question()
                 self.message = None
                 self.message_timeout = 0
         if self.q:
@@ -193,34 +263,12 @@ if __name__ == '__main__':
     waitForPlayerToPressKey()
     g.q = Question()
     done=False
-    user_input = ''
-    while done==False:
+
+    while g.state!=QUIT:
         time_chunk = g.clk.tick(20)
-        for event in pygame.event.get(): # User did something
-            if event.type == pygame.QUIT: # If user clicked close
-                done=True # Flag that we are done so we exit this loop
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE: # pressing escape quits
-                    done=True # Flag that we are done so we exit this loop
-                    break
-                for key, char in keynum.iteritems():
-                    if event.key == key: 
-                        user_input += keynum[key]
-                if event.key == pygame.K_RETURN or event.key == K_NUMPAD_RETURN: 
-                    if user_input != '':
-                        g.a = user_input
-                        user_input = ''
-                        break
-                print event
-            
         g.render()
-        g.update(user_input)
+        g.update()
         pygame.display.flip()
     
     pygame.quit()
     sys.exit()
-
-    
-        
-
-    
